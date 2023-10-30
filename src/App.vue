60591b458f49e6371e03ae73a669f85f94a1ec33
Tomáš Sabol <tomassabol2@gmail.com>
2023-10-30 11:55:02 +0100

    added dropdown menu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import Button from "./components/Button.vue";
+import DropDownMenu from "./components/DropDownMenu.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="w-screen h-screen gap-2 items-center justify-center flex flex-col"
+  >
+    <div class="flex gap-2">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+    <Button @click.prevent="console.log('shadcn')" />
+    <DropDownMenu />
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <Button @click.prevent="console.log('shadcn')" />
 </template>
